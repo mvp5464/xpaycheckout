61be7ec5ed7aa32bcf1917130ee2fe970f3d7a3a
mvking5464 <mvking5464@gmail.com>
2024-06-10 21:18:49 +0530

    PricingSection Added

--- src/components/PricingCard.tsx
+++ src/components/PricingCard.tsx
@@ -1,32 +1,38 @@
-const PricingCard = ({ type }: { type: "basic" | "enterprise" }) => {
+import { ReactNode } from "react";
+
+type color = "white" | "[#141414]";
+
+const PricingCard = ({
+  textColor,
+  bgColor,
+  title,
+  body,
+  tickLine,
+}: {
+  textColor: color;
+  bgColor: color;
+  title: string;
+  body: string;
+  tickLine: ReactNode;
+}) => {
   return (
     <div>
-      <div>
-        <div>{type === "basic" ? "Basic" : "Enterprise"}</div>
-        <div>
-          {type === "basic"
-            ? "Onboard. Integrate. Start collecting USD in 24 hours."
-            : "White Glovemanaged solution for teams powering the world"}
+      <div className={`text-${textColor} bg-${bgColor} p-8 rounded-xl`}>
+        <div className="font-semibolds text-[1.7rem] mb-2">{title}</div>
+        <div className="text-lg">{body}</div>
+        <div
+          className={`py-5 flex gap-3 items-center ${
+            bgColor === "[#141414]" && "invisible"
+          }`}
+        >
+          <span className="text-6xl">5%</span>/transaction
         </div>
-        <div>{type === "basic" ? "5%/transaction" : ""}</div>
-        <button>
-          Get in Touch for {type === "basic" ? "Enterprise" : "Pricing"}
+        <button
+          className={`w-full text-center rounded-full border border-white bg-white text-[#3d3b3b] hover:bg-[#141414] hover:text-white py-2 mb-10 font-semibold shadow-stone-700`}
+        >
+          Get in Touch for Pricing
         </button>
-        {type === "basic" ? (
-          <>
-            <div>(tick) Accept payments in USD</div>
-            <div>(tick) Collect from 100+ countries</div>
-            <div>(tick) Get money in your INR account</div>
-            <div>(tick) Create Global Payment Links</div>
-          </>
-        ) : (
-          <>
-            <div>(tick) Everything in Basic plus</div>
-            <div>(tick) Accept payments in 30+ currencies</div>
-            <div>(tick) Managed Sales tax solution</div>
-            <div>(tick) High priority addition of new payment methods</div>
-          </>
-        )}
+        <div className="flex flex-col gap-3 mb-32">{tickLine}</div>
       </div>
     </div>
   );
--- src/components/Sections/Pricing.tsx
+++ src/components/Sections/Pricing.tsx
@@ -1,15 +1,55 @@
 import PricingCard from "../PricingCard";
+import TickLine from "../TickLine";
 import Wrapper from "../Wrapper";
 
 const Pricing = () => {
   return (
     <div>
       <Wrapper>
-        <div className="bg-green-400">
-          <div>Premium features without premium pricing</div>
-          <div className=" flex">
-            <PricingCard type={"basic"} />
-            <PricingCard type={"enterprise"} />
+        <div className=" py-20">
+          <div className=" font-[550] text-5xl text-center leading-tight mb-16">
+            Premium features without premium pricing
+          </div>
+          <div className=" grid grid-cols-2 gap-10">
+            <PricingCard
+              textColor={"[#141414]"}
+              bgColor={"white"}
+              title={"Basic"}
+              body={"Onboard. Integrate. Start collecting USD in 24 hours."}
+              tickLine={
+                <>
+                  <TickLine bgWhite={true} text={"Accept payments in USD"} />
+                  <TickLine
+                    bgWhite={true}
+                    text={"Collect from 100+ countries"}
+                  />
+                  <TickLine
+                    bgWhite={true}
+                    text={"Get money in your INR account"}
+                  />
+                  <TickLine
+                    bgWhite={true}
+                    text={"Create Global Payment Links"}
+                  />
+                </>
+              }
+            />
+            <PricingCard
+              textColor={"white"}
+              bgColor={"[#141414]"}
+              title={"Enterprise"}
+              body={"White Glove managed solution for teams powering the world"}
+              tickLine={
+                <>
+                  <TickLine text={"Everything in Basic plus"} />
+                  <TickLine text={"Accept payments in 30+ currencies"} />
+                  <TickLine text={"Managed Sales tax solution"} />
+                  <TickLine
+                    text={"High priority addition of new payment methods"}
+                  />
+                </>
+              }
+            />
           </div>
         </div>
       </Wrapper>
