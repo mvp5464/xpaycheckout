2df8cf77fd9e0076f6c333add4f5bfb901324c8f
mvking5464 <mvking5464@gmail.com>
2024-06-11 00:05:54 +0530

    InfoSection Added

--- src/components/Sections/InfoSection.tsx
+++ src/components/Sections/InfoSection.tsx
@@ -1,31 +1,65 @@
+import Image from "next/image";
 import Wrapper from "../Wrapper";
 
 const InfoSection = () => {
   return (
     <div>
       <Wrapper>
-        <div className=" bg-fuchsia-400">
-          <div className=" grid grid-cols-2">
-            <div>image</div>
-            <div className=" grid grid-cols-3">
+        <div className=" border-b border-dashed mb-9">
+          <div className=" grid grid-cols-2 gap-3 py-14">
+            <div>
+              <Image
+                src={"/xPayBlackIcon.avif"}
+                alt={"Logo"}
+                width={150}
+                height={50}
+              />
+            </div>
+            <div className=" grid grid-cols-3 gap-3">
               <div>
-                <div>xPay</div>
-                <div>Documentation</div>
-                <div>Waitlist</div>
-                <div>Pricing</div>
+                <div className=" text-lg font-semibold mb-4">xPay</div>
+                <div className=" text-sm mb-2">Documentation</div>
+                <div className=" text-sm mb-2">Waitlist</div>
+                <div className=" text-sm mb-2">Pricing</div>
               </div>
               <div>
-                <div>Policies</div>
-                <div>Terms and Conditions</div>
-                <div>Privacy Policy</div>
-                <div>Shipping Policy</div>
-                <div>Cancellation and Refund Policy</div>
+                <div className=" text-lg font-semibold mb-4">Policies</div>
+                <div className=" text-sm mb-2">Terms and Conditions</div>
+                <div className=" text-sm mb-2">Privacy Policy</div>
+                <div className=" text-sm mb-2">Shipping Policy</div>
+                <div className=" text-sm mb-2">
+                  Cancellation and Refund Policy
+                </div>
               </div>
               <div>
-                <div>Contact</div>
-                <div>(image) LinkedIn</div>
-                <div>(image) Email</div>
-                <div>(image) Talk to Founder</div>
+                <div className=" text-lg font-semibold mb-4">Contact</div>
+                <div className=" text-sm mb-1 flex justify-start items-center gap-2">
+                  <Image
+                    src={"/linkedinIcon.avif"}
+                    alt={"Logo"}
+                    width={25}
+                    height={25}
+                  />
+                  <span>LinkedIn</span>
+                </div>
+                <div className=" text-sm mb-1 flex justify-start items-center gap-2">
+                  <Image
+                    src={"/emailIcon.avif"}
+                    alt={"Logo"}
+                    width={25}
+                    height={25}
+                  />
+                  <span>Email</span>
+                </div>
+                <div className=" text-sm mb-1 flex justify-start items-center gap-2 ">
+                  <Image
+                    src={"/phoneIcon.avif"}
+                    alt={"Logo"}
+                    width={25}
+                    height={25}
+                  />
+                  <span>Talk to Founder</span>
+                </div>
               </div>
             </div>
           </div>
