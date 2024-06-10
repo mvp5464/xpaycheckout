e94c12d0daf9e6345a46206c12c946aac492f6aa
mvking5464 <mvking5464@gmail.com>
2024-06-10 10:50:12 +0530

    added appbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className=" bg-slate-300 w-screen h-screen overflow-x-hidden">
+        <div className=" bg-slate-300 w-screen h-screen overflow-x-hidden font-sans">
           {children}
         </div>
       </body>
--- src/components/Sections/Appbar.tsx
+++ src/components/Sections/Appbar.tsx
@@ -1,18 +1,23 @@
+import Image from "next/image";
 import Wrapper from "../Wrapper";
+import RightArrow from "../icons/RightArrow";
 
 const Appbar = () => {
   return (
-    <div>
-      {/* <div className=""> */}
+    <div className=" bg-black">
       <Wrapper>
-        <div className=" flex justify-between items-center h-20 bg-fuchsia-400">
-          <div>pay</div>
+        <div className=" flex justify-between items-center h-20 text-white ">
+          <div>
+            <Image src={"/Logo.avif"} alt={"Logo"} width={100} height={50} />
+          </div>
           <div className=" flex gap-4">
             <button>Pricing</button>
             <button>Waitlist</button>
             <button>Contact</button>
             <button>Blogs</button>
-            <button>Talk to founder (arrow)</button>
+            <button className=" flex bg-white text-black rounded-full py-3 px-6">
+              <span>Talk to founder</span> <RightArrow />
+            </button>
           </div>
         </div>
       </Wrapper>
